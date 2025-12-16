ac31f521046b989521e9a1ee2b8c6929dfd1baaa
Vitaliy Mikhaylov <vitaly.mikhaylov94@yandex.ru>
2025-12-17 02:10:21 +0500

    add database schema

---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS persons;
+
+CREATE TABLE persons
+(
+    name           VARCHAR(50) NOT NULL,
+    surname        VARCHAR(50) NOT NULL,
+    age            INT         NOT NULL,
+    phone_number   VARCHAR(20),
+    city_of_living VARCHAR(50),
+    PRIMARY KEY (name, surname, age)
+);
